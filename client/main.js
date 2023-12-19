e27e9442fe26f84be330cba53f8724218ccbca2e
juhyuny <juhyun5358@gmail.com>
2023-12-19 13:06:13 +0900

    05. 랜덤주사위

--- client/main.js
+++ client/main.js
@@ -1,1 +1,20 @@
-alert('안녕 자바스크립트!');
+import { getNodes } from "./lib/index.js";
+
+
+
+
+const [rollingButton,recordButton,resetButton] = getNodes('.buttonGroup > button');
+
+
+// [phase-1]
+// 1. 주사위 굴리기
+// 2. 주사위 굴리기 버튼을 클릭하면 diceAnimation() 실행될 수 있도록
+
+
+
+function handleRollingDice(){
+
+}
+
+
+rollingButton.addEventListener('click',handleRollingDice)
